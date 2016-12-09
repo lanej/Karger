c549950f087b158028ca1d5a5b360ba97f647709
Joshua Lane <me@joshualane.com>
2016-12-08 20:52:51 -0800

    mincut algroith working
    though efficiency is a problem.  I might have to rework the modeling to
    allow for easier edge deletion.  maybe store it as a mapt

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 // Graph represents a collection of nodes
 type Graph struct {
 	Nodes map[int]*Node
+	Edges []*Edge
 }
 
 // Edge represents to a connection between two nodes
@@ -29,6 +33,10 @@ func NewGraph() *Graph {
 
 // Add a node to the graph
 func (g *Graph) Add(label int) *Node {
+	existing := g.Nodes[label]
+	if existing != nil {
+		return existing
+	}
 	node := &Node{Label: label, Graph: g}
 	g.Nodes[label] = node
 	return node
@@ -44,6 +52,76 @@ func (g *Graph) Connect(a, b *Node) *Edge {
 
 	a.Edges = append(a.Edges, edge)
 	b.Edges = append(b.Edges, edge)
+	g.Edges = append(g.Edges, edge)
 
 	return edge
+}
+
+// Reconnect edge
+func (from *Node) Reconnect(to *Node, not *Edge) {
+	for _, edge := range from.Edges {
+		if edge == not {
+			// do not process contracting edge
+		} else if edge.destination == from {
+			edge.destination = to
+			to.Edges = append(to.Edges, edge)
+		} else {
+			edge.origin = to
+			to.Edges = append(to.Edges, edge)
+		}
+	}
+}
+
+// Contract an edge
+func (e *Edge) Contract() *Node {
+	origin := e.origin
+	destination := e.destination
+	graph := origin.Graph
+
+	supernode := graph.Add(origin.Label + destination.Label)
+
+	origin.Reconnect(supernode, e)
+	destination.Reconnect(supernode, e)
+
+	delete(graph.Nodes, origin.Label)
+	delete(graph.Nodes, destination.Label)
+
+	var edgeIndex int
+	for index, gEdge := range graph.Edges {
+		if gEdge == e {
+			edgeIndex = index
+			break
+		}
+	}
+
+	graph.Edges = append(graph.Edges[:edgeIndex], graph.Edges[(edgeIndex+1):]...)
+
+	return supernode
+}
+
+// MinCut returns the size of the calculated MinCut using Krager's Random Contraction algorithm
+func (g *Graph) MinCut() int {
+	var index int
+	var edge *Edge
+
+	for len(g.Nodes) > 2 {
+		index = rand.Intn(len(g.Edges))
+		edge = g.Edges[index]
+
+		edge.Contract()
+
+		fmt.Printf("nodes remaining: %v, edges remaining: %v\n", len(g.Nodes), len(g.Edges))
+	}
+
+	return len(g.Edges)
+}
+
+func (g *Graph) print() {
+	for index, edge := range g.Edges {
+		fmt.Printf("Edge #%v: [%v, %v]\n", index, edge.destination.Label, edge.origin.Label)
+	}
+}
+
+func (e *Edge) print() {
+	fmt.Printf("Edge [%v, %v]\n", e.destination.Label, e.origin.Label)
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -34,12 +39,128 @@ func TestGraphConnect(t *testing.T) {
 	assert.Contains(t, node2.Edges, edge)
 }
 
-// func TestNodeContract(t *testing.T) {
-// 	graph := NewGraph()
-//
-// 	node1 := graph.Add(1)
-// 	node2 := graph.Add(2)
-// 	node3 := graph.Add(3)
-// 	node4 := graph.Add(4)
-//
-// }
+func TestNodeContract1(t *testing.T) {
+	graph := NewGraph()
+
+	node1 := graph.Add(1)
+	node2 := graph.Add(2)
+	node3 := graph.Add(3)
+
+	graph.Connect(node1, node2)
+	graph.Connect(node2, node3)
+	edge := graph.Connect(node3, node1)
+
+	supernode := edge.Contract()
+	assert.Equal(t, 4, supernode.Label, "Failed to add the labels together")
+
+	var vertices []int
+
+	for _, edge := range supernode.Edges {
+		vertices = []int{edge.origin.Label, edge.destination.Label}
+		sort.Ints(vertices)
+		assert.Equal(t, []int{2, 4}, vertices, "Edge contraction failed")
+	}
+}
+
+func TestNodeContact2(t *testing.T) {
+	graph := NewGraph()
+
+	node1 := graph.Add(1)
+	node2 := graph.Add(2)
+	node3 := graph.Add(3)
+	node4 := graph.Add(4)
+
+	graph.Connect(node1, node3)
+	edge1 := graph.Connect(node2, node3)
+	edge2 := graph.Connect(node4, node3)
+
+	edge1.Contract()
+
+	graph.print()
+
+	edge2.Contract()
+
+	graph.print()
+}
+
+func TestMinCut1(t *testing.T) {
+	graph := NewGraph()
+
+	node1 := graph.Add(1)
+	node2 := graph.Add(2)
+	node3 := graph.Add(3)
+
+	graph.Connect(node1, node2)
+	graph.Connect(node2, node3)
+	graph.Connect(node3, node1)
+
+	minCutSize := graph.MinCut()
+
+	assert.Equal(t, 2, minCutSize, "Invalid MinCut result")
+}
+
+func TestMinCut2(t *testing.T) {
+	graph := NewGraph()
+
+	node1 := graph.Add(1)
+	node2 := graph.Add(2)
+	node3 := graph.Add(3)
+	node4 := graph.Add(4)
+
+	graph.Connect(node1, node3)
+	graph.Connect(node2, node3)
+	graph.Connect(node4, node3)
+
+	minCutSize := graph.MinCut()
+
+	assert.Equal(t, 1, minCutSize, "Invalid MinCut result")
+}
+
+func TestHomework(t *testing.T) {
+	input := integersFromFile("mincut.txt")
+	graph := NewGraph()
+
+	for _, row := range input {
+		node := graph.Add(row[0])
+		for _, i := range row[1:] {
+			relative := graph.Add(i)
+			graph.Connect(node, relative)
+		}
+	}
+
+	minCutSize := graph.MinCut()
+
+	fmt.Printf("MinCut Size: %v", minCutSize)
+}
+
+func integersFromFile(s string) [][]int {
+	inputBytes, err := ioutil.ReadFile(s)
+
+	if err != nil {
+		panic(err)
+	}
+
+	inputString := string(inputBytes[:])
+	rawRows := strings.Split(inputString, "\n")
+	data := make([][]int, len(rawRows)-1)
+
+	for row, i := range rawRows {
+		values := strings.Split(i, "\t")
+
+		cols := []int{}
+
+		for _, v := range values {
+			j, err := strconv.Atoi(strings.TrimSpace(v))
+
+			if err == nil {
+				cols = append(cols, j)
+			}
+		}
+
+		if len(cols) > 0 {
+			data[row] = cols
+		}
+	}
+
+	return data
+}
